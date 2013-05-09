cb9ece9854b58eb87dfa3b192139e78ad51dd80d
CJ <weicongju@gmail.com>
2013-05-09 11:15:50 +0800

    Use mysql.

--- mdapp/settings.py
+++ mdapp/settings.py
@@ -41,7 +41,6 @@ if MYSQL_HOST:
             'PORT': MYSQL_PORT,
         }
     }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
