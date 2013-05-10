ea801d69deedb397a1dec3bb676ca046d7e11548
CJ <weicongju@gmail.com>
2013-05-10 22:26:49 +0800

    BUG

--- md/urls.py
+++ md/urls.py
@@ -9,7 +9,7 @@ message_handler = Resource(MessageHandler, authentication= auth)
 chat_handler = Resource(ChatHandler, authentication=auth)
 chats_handler = Resource(ChatsHandler, authentication=auth)
 urlpatterns = oauthost.urls.urlpatterns + patterns('',
-    url(r'^authentication/$', 'auth_views.endpoint_token',\
+    url(r'^authentication/$', 'oauthost.auth_views.endpoint_token',\
         name='oauthost_token'),
     url(r'^message/$', message_handler),
     url(r'^chat/$', chat_handler),
