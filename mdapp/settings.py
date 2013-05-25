679de432a67f577420117246e68a3ab2f75430f7
CJ <weicongju@gmail.com>
2013-05-25 23:55:30 +0800

    media root的环境变量。

--- mdapp/settings.py
+++ mdapp/settings.py
@@ -70,7 +70,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(WEB_ROOT, "media")
+MEDIA_ROOT = os.getenv('HIMAIDUO_MEDIA_ROOT', os.path.join(WEB_ROOT, "media"))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
