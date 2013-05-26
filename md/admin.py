56626679753f1357750130c00ccc5db0e6d5ffeb
CJ <weicongju@gmail.com>
2013-05-26 22:47:10 +0800

    后台加入Chat和Message的数据管理。

--- md/admin.py
+++ md/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Activity, Chat
+from models import Activity, Chat, Message, MessageAddon
 
 class ActivityAdmin(admin.ModelAdmin):
     pass
@@ -7,5 +7,13 @@ class ActivityAdmin(admin.ModelAdmin):
 class ChatAdmin(admin.ModelAdmin):
     pass
 
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+class MessageAddonAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Activity, ActivityAdmin)
 admin.site.register(Chat, ChatAdmin)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(MessageAddon, MessageAddonAdmin)
