0960205a1d9313e07692530d4fc2d389ca26494d
cj <cj@imcj.me>
2013-08-04 23:36:09 +0800

    更新去掉中间的迁移脚本。

--- md/migrations/0013_auto__add_user__add_field_messageaddon_key__add_field_messageaddon_wid.py
+++ md/migrations/0013_auto__add_user__add_field_messageaddon_key__add_field_messageaddon_wid.py
@@ -8,6 +8,41 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'User'
+        db.create_table(u'md_user', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('is_superuser', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('username', self.gf('django.db.models.fields.CharField')(unique=True, max_length=30)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30, blank=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
+            ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('date_joined', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('mobile', self.gf('django.db.models.fields.CharField')(unique=True, max_length=11, db_index=True)),
+        ))
+        db.send_create_signal(u'md', ['User'])
+
+        # Adding M2M table for field groups on 'User'
+        m2m_table_name = db.shorten_name(u'md_user_groups')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('user', models.ForeignKey(orm[u'md.user'], null=False)),
+            ('group', models.ForeignKey(orm[u'auth.group'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['user_id', 'group_id'])
+
+        # Adding M2M table for field user_permissions on 'User'
+        m2m_table_name = db.shorten_name(u'md_user_user_permissions')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('user', models.ForeignKey(orm[u'md.user'], null=False)),
+            ('permission', models.ForeignKey(orm[u'auth.permission'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['user_id', 'permission_id'])
+
         # Adding field 'MessageAddon.key'
         db.add_column(u'md_messageaddon', 'key',
                       self.gf('django.db.models.fields.CharField')(default='', max_length=255),
@@ -29,7 +64,34 @@ class Migration(SchemaMigration):
                       keep_default=False)
 
 
+        # Changing field 'Message.user'
+        db.alter_column(u'md_message', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['md.User']))
+
+        # Changing field 'ActivityInvite.user'
+        db.alter_column(u'md_activityinvite', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['md.User']))
+
+        # Changing field 'ActivityInvite.refer'
+        db.alter_column(u'md_activityinvite', 'refer_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['md.User']))
+
+        # Changing field 'Chat.user'
+        db.alter_column(u'md_chat', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['md.User']))
+
+        # Changing field 'UserToken.user'
+        db.alter_column(u'md_usertoken', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['md.User']))
+
+        # Changing field 'Activity.owner'
+        db.alter_column(u'md_activity', 'owner_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['md.User']))
+
     def backwards(self, orm):
+        # Deleting model 'User'
+        db.delete_table(u'md_user')
+
+        # Removing M2M table for field groups on 'User'
+        db.delete_table(db.shorten_name(u'md_user_groups'))
+
+        # Removing M2M table for field user_permissions on 'User'
+        db.delete_table(db.shorten_name(u'md_user_user_permissions'))
+
         # Deleting field 'MessageAddon.key'
         db.delete_column(u'md_messageaddon', 'key')
 
@@ -42,6 +104,24 @@ class Migration(SchemaMigration):
         # Deleting field 'MessageAddon.size'
         db.delete_column(u'md_messageaddon', 'size')
 
+
+        # Changing field 'Message.user'
+        db.alter_column(u'md_message', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User']))
+
+        # Changing field 'ActivityInvite.user'
+        db.alter_column(u'md_activityinvite', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User']))
+
+        # Changing field 'ActivityInvite.refer'
+        db.alter_column(u'md_activityinvite', 'refer_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User']))
+
+        # Changing field 'Chat.user'
+        db.alter_column(u'md_chat', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User']))
+
+        # Changing field 'UserToken.user'
+        db.alter_column(u'md_usertoken', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User']))
+
+        # Changing field 'Activity.owner'
+        db.alter_column(u'md_activity', 'owner_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User']))
 
     models = {
         u'auth.group': {
@@ -57,22 +137,6 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        u'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
         u'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
@@ -85,10 +149,10 @@ class Migration(SchemaMigration):
             'create_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'ip': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39'}),
-            'owner': ('django.db.models.fields.related.ForeignKey', [], {'default': '0', 'to': u"orm['auth.User']"}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'default': '0', 'to': u"orm['md.User']"}),
             'subject': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'update_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
-            'user': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'participate'", 'symmetrical': 'False', 'to': u"orm['auth.User']"})
+            'user': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'participate'", 'symmetrical': 'False', 'to': u"orm['md.User']"})
         },
         u'md.activityinvite': {
             'Meta': {'object_name': 'ActivityInvite'},
@@ -96,8 +160,8 @@ class Migration(SchemaMigration):
             'avaiable': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'create_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'refer': ('django.db.models.fields.related.ForeignKey', [], {'default': '0', 'related_name': "'refers'", 'to': u"orm['auth.User']"}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'default': '0', 'related_name': "'users'", 'to': u"orm['auth.User']"})
+            'refer': ('django.db.models.fields.related.ForeignKey', [], {'default': '0', 'related_name': "'refers'", 'to': u"orm['md.User']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'default': '0', 'related_name': "'users'", 'to': u"orm['md.User']"})
         },
         u'md.chat': {
             'Meta': {'ordering': "['-id']", 'object_name': 'Chat'},
@@ -106,7 +170,7 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'ip': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39'}),
             'text': ('django.db.models.fields.CharField', [], {'max_length': '4000'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['md.User']"})
         },
         u'md.message': {
             'Meta': {'ordering': "['-create_at']", 'object_name': 'Message'},
@@ -119,11 +183,11 @@ class Migration(SchemaMigration):
             'message_type': ('django.db.models.fields.CharField', [], {'default': "'Text'", 'max_length': '1'}),
             'stash': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'update_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['md.User']"})
         },
         u'md.messageaddon': {
             'Meta': {'object_name': 'MessageAddon'},
-            'create_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 6, 27, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
+            'create_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 8, 4, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
             'extra': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'height': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
@@ -141,13 +205,30 @@ class Migration(SchemaMigration):
             'ip': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39'}),
             'mobile': ('django.db.models.fields.TextField', [], {})
         },
+        u'md.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'mobile': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '11', 'db_index': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
         u'md.usertoken': {
             'Meta': {'object_name': 'UserToken'},
             'create_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'ip': ('django.db.models.fields.GenericIPAddressField', [], {'max_length': '39'}),
             'token': ('django.db.models.fields.CharField', [], {'max_length': '6'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['md.User']"})
         }
     }
 
